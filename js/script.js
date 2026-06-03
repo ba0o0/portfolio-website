bd5ba86cec039e50556986884e35e7b77a6686b4
ba0o0o <bhtp2004@gmail.com>
2026-06-03 00:41:53 -0400

    worked on about(added current listening to image from lastfm)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 
+setInterval(() => {
+  document.querySelector('.lastfm img').src = 
+    'https://lastfm-recently-played.vercel.app/api?user=ba0o0o&count=1&width=500&height=1000' + Date.now();
+}, 10000); // updates every 30 seconds

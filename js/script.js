c581e10243c9acc3f9cc68d5fd2654ed42bd3ea4
Baohuan-Tran <btran7@umbc.edu>
2026-06-04 04:11:26 -0400

    contact button links to gmail replaced about section as home and created a new section called about

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 
+const myGmail = "https://mail.google.com/mail/?view=cm&to=bhtp2004@gmail.com";
+const gmailElement = document.getElementById("myGmail");
+gmailElement.href = myGmail;
+
+
 setInterval(() => {
   document.querySelector('.lastfm img').src = 
     'https://lastfm-recently-played.vercel.app/api?user=ba0o0o&count=1&width=500&height=1000' + Date.now();
 }, 10000); // updates every 30 seconds
+

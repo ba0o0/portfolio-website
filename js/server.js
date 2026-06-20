594645b9e961fe46fde6475ed93b8bf91d8039ea
ba0o0o <bhtp2004@gmail.com>
2026-06-20 17:06:26 -0400

    worked on backend

--- js/server.js
+++ js/server.js
@@ -5,8 +5,41 @@ const PORT =  8080;
 app.get('/', (req,res) => {
     res.send("GET: Hello from Express");
     
-})
+});
 
-app.listen(PORT, () => {
+app.get('/about', (req, res) => {
+    res.send('This is the about page')
+});
+
+app.get('/products', (req, res) => {
+    res.json([
+        {id: 0, name: 'Chud', date:"09/10/2004 @ 7:38 pm"},
+        {id: 1, name: 'oba', date:"09/11/2004 @ 7 pm"}
+    ])
+});
+
+app.get('/products/:id', (req, res) => {
+    const id = Number (req.params.id);
+    const product = [
+        {id: 0, name: 'Chud', date:"09/10/2004 @ 7:38 pm"},
+        {id: 1, name: 'oba', date:"09/11/2004 @ 7 pm"}
+    ]   
+
+    const requestedProduct = product.find((product) => product.id === id);
+    res.json(requestedProduct);
+});
+
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/contact', (req, res) => {
+    const email = req.body.email;
+    console.log('Email recieved' , email);
+
+    res.send('Email submitted successfully!')
+});
+
+app.listen(PORT, () => {    
     console.log("Backend Server is running on port: 8080");
 })
